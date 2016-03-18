03a53e608eb863a3f093678082886500b2fc2c6d
Tim <timinth@gmail.com>
2016-03-18 11:03:59 -0700

    updated the waking script to be able to work for a specific 5-inch screen

--- wakeMonitor.sh
+++ wakeMonitor.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-tvservice --preferred
+tvservice -e "DMT 87 DVI" # this is specificially for the 5" screen
+# add this back in for normal monitors tvservice --preferred
 fbset -depth 8
 fbset -depth 16
 xrefresh
